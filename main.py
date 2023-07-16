777358b1816ce93d0ba24de9b7cf697d6b362165
Juhyun <juhyunk1119@gmail.com>
2023-07-16 11:20:02 +0900

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+
+TOKEN = "MTEyOTk0NzgzNzQ3NjYzNDY1NQ.GHfjP9.MVWLa_X1jQ2GtjBHbA73nofPE-rVBrUpQoKL90"
+channelID = []
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'loggen in as {self.user}')
+        
+    async def on_message(self, message):
+        print(f'message from {message.author}: {message.content}')
+        #if message.channel.type == "public_thread":
+        print(message.channel.id)
+        
+        if message.author.bot == False and str(message.channel.type) == "public_thread":
+            if message.channel.id in channelID:
+                pass
+            else:
+                await message.channel.send("<@&1077985851562278922>")
+                channelID.append(message.channel.id)
+        
+    
+        
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
+
+
+
